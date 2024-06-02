7bc847885de29940630b768fc07cc2d5c0823a36
Nick <169969679+bamboofeather@users.noreply.github.com>
2024-06-02 11:13:18 +0200

    Add sail control

--- Assets/Sailing Minigame/SailController.cs
+++ Assets/Sailing Minigame/SailController.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class SailController : MonoBehaviour
 {
     // Level specifing to what extend the sail is hoisted
+    public GameObject sail;
     [Range(0.0f, 1.0f)]
-    public float level = 0;
+    public float level = 0.5f;
+    [Range(0.0f, 1.0f)]
+    public float levelTarget = 0;
+    public GameObject gaugeIndicator;
     [Range(0.0f, 1.0f)]
     public float gauge = 0;
     [Range(0.0f, 1.0f)]
@@ -14,26 +18,48 @@ public class SailController : MonoBehaviour
     //Delay between a target change in seconds
     public int targetChangeDelay = 3;
     public float speed = 0.0001f;
+    public int updateSpeed = 1;
 
-    private IEnumerator coroutine;
+    private List<IEnumerator> coroutines;
+    private float lastGauge;
+    private float lastOrientation;
+    public float currentOrientation;
+    private float lastLevelTarget;
+    private Quaternion sailStartRotation;
+    private Vector3 gaugeStartPosition;
     
     
     // Start is called before the first frame update
     void Start()
     {
-        coroutine = NextGaugeTarget(targetChangeDelay);
-        StartCoroutine(coroutine);
+        coroutines = new List<IEnumerator>();
+        coroutines.Add(NextGaugeTarget(targetChangeDelay));
+        coroutines.Add(UpdateLevelTarget());
+
+        foreach(IEnumerator coroutine in coroutines)
+            StartCoroutine(coroutine);
+
+        sailStartRotation = sail.transform.rotation;
+        gaugeStartPosition = gaugeIndicator.transform.position;
+        // sail.transform.rotation = 30;
+        // lastLevel = level;
     }
 
     // Update is called once per frame
     void Update()
     {
         gauge = CalculateGauge(speed);
+        level = CalculateLevel(updateSpeed);
+        // gaugeIndicator.transform.position = new Vector3(-7, 2+(gauge*3), 0);
+        // sail.transform.position = new Vector3(0, 4+(level*3), 0);
+        sail.transform.rotation = sailStartRotation * Quaternion.AngleAxis((sailStartRotation.y -30) + (60*level), Vector3.forward);
+        gaugeIndicator.transform.position = new Vector3((gaugeStartPosition.x-5) + (10*gauge), gaugeStartPosition.y, gaugeStartPosition.z);
     }
 
     void OnDestroy()
     {
-        StopCoroutine(coroutine);
+        foreach(IEnumerator coroutine in coroutines)
+            StopCoroutine(coroutine);
     }
 
 
@@ -53,11 +79,34 @@ public class SailController : MonoBehaviour
         return newGauge;
     }
 
+    private float CalculateLevel(float updateSpeed)
+    {
+        float newLevel = level;
+        if(level < levelTarget)
+            newLevel += speed;
+        else if (level > levelTarget)
+            newLevel -= speed;
+        else
+            newLevel = levelTarget;
+        
+        return newLevel;
+    }
+
     // Adjust the current sail level based on cube orientation
     public void HandleOrientation(float orientation)
     {
         float degrees = orientation + 180;
-        level = degrees/360;
+        // if (lastOrientation == null)
+        //     lastOrientation = degrees;
+
+        // if (Mathf.Abs(lastOrientation-degrees) < 2)
+        // {
+            // lastOrientation = lastOrientation > 250 ? 360 : 0;
+            currentOrientation = degrees;
+        // } else
+            // lastOrientation = degrees;
+        // }
+        // level = lastOrientation/360;
     }
 
     // Choose a new target
@@ -68,4 +117,44 @@ public class SailController : MonoBehaviour
             yield return new WaitForSeconds(delay);
         }
     }
+
+    private IEnumerator UpdateLevelTarget()
+    {
+        lastOrientation = currentOrientation;
+
+        while(true)
+        {
+            float orientationDifference = lastOrientation - currentOrientation;
+
+            // Prevent rotation reset, e.g. when cube goes from rot 290 to 30 in the given time frame
+            if(lastOrientation < 100 && currentOrientation > 250)
+            {
+                orientationDifference = currentOrientation - (lastOrientation + 360);
+            } else if(lastOrientation > 250 && currentOrientation < 100)
+            {
+                orientationDifference = (currentOrientation + 360) - lastOrientation;
+            }
+
+            // normalize orientationDifference
+            orientationDifference = orientationDifference/360;
+            float newLevelTarget = levelTarget + orientationDifference;
+
+            // Clamp target Level between 0 and 1
+            if(0 <= newLevelTarget && newLevelTarget <= 1)
+                levelTarget = newLevelTarget;
+
+            // Update last orientation
+            lastOrientation = currentOrientation;
+
+            yield return new WaitForSeconds(updateSpeed);
+        }
+    }
+
+    // private IEnumerator UpdateLevel(int updateSpeed)
+    // {
+    //     while(true){
+    //         level = ;
+    //         yield return new WaitForSeconds(updateSpeed);
+    //     }
+    // }
 }
